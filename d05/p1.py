9b5953deb4632fe967ff5d892c0822960210782a
Luca Lumetti <lumetti.luca@gmail.com>
2020-12-05 15:02:48 +0100

    d05 test->inpy

--- d05/p1.py
+++ d05/p1.py
@@ -1,2 +1,2 @@
-f = open('test','r').readlines()
+f = open('input','r').readlines()
 print(max([ int(l.replace('F','0').replace('B','1')[:-4],2)*8+int(l.replace('R','1').replace('L','0')[-4:],2) for l in f ]))
--- d05/p2.py
+++ d05/p2.py
@@ -1,3 +1,3 @@
-f = open('test','r').readlines()
+f = open('input','r').readlines()
 s = [ int(l.replace('F','0').replace('B','1')[:-4],2)*8+int(l.replace('R','1').replace('L','0')[-4:],2) for l in f ]
 print((set(range(min(s),max(s))) - set(s)).pop())
