8a3136b7c71b9954e0cb52d3f68e1d95ccf0f30e
Luca Lumetti <lumetti.luca@gmail.com>
2020-12-18 14:28:45 +0100

    less code for d18

--- d18/p1-2.py
+++ d18/p1-2.py
@@ -1,18 +1,16 @@
 import re
 
 class I1(int):
-    def __init__(self, a):
-        self.a = a
     def __add__(self, b):
-        return I1(self.a + b.a)
+        return I1(self.real + b.real)
     def __sub__(self, b):
-        return I1(self.a * b.a)
+        return I1(self.real * b.real)
 
-class I2(I1):
+class I2(int):
     def __add__(self, b):
-        return I2(self.a * b.a)
+        return I2(self.real * b.real)
     def __mul__(self, b):
-        return I2(self.a + b.a)
+        return I2(self.real + b.real)
 
 f1 = [ l.replace('*','-') for l in open('input').readlines()]
 print(sum([eval(re.sub(r"(\d+)", r"I1(\1)", l)) for l in f1]))
