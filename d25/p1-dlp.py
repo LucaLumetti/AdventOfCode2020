5ae7b856b5cd0ae0a2b59c8f7cfeb181a7e42092
Luca Lumetti <lumetti.luca@gmail.com>
2020-12-25 15:42:45 +0100

    remove time

--- d25/p1-dlp.py
+++ d25/p1-dlp.py
@@ -1,5 +1,4 @@
 from math import ceil, sqrt
-import time
 
 def bsgs(g, h, p):
     N = ceil(sqrt(p - 1))
@@ -11,7 +10,5 @@ def bsgs(g, h, p):
             return j * N + tbl[y]
     return None
 
-START = time.time()
 c_pk, s_pk = [ int(l.rstrip()) for l in open('input').readlines() ]
 print(pow(s_pk, bsgs(7, c_pk, 20201227), 20201227))
-print(f"TIME: {time.time()-START}")
